a92808cdb92ac94e3f7258d61e0bf74da5cd0c4e
Swong87 <8swordz@gmail.com>
2017-08-11 22:51:22 -0400

    JAWS

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 var myPassword = require("../myPassword.js");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: myPassword,
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: myPassword,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
